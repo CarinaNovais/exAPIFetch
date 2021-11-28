38750ef4a76ea01f92b0a408ce73a6718d0e0719
CarinaNovais <carinapereiraN@outlook.com>
2021-11-28 22:20:13 +0900

    Arrumando função de conexão

--- www/lib/script.js
+++ www/lib/script.js
@@ -8,157 +8,181 @@ window.onload = function (){
   const deletar = document.querySelector("#deletar");
   const exibir = document.querySelector("#buscarQR");
 
+
+  const opcoes = {
+    method:'GET',
+    mode:'cors',
+    cache: 'default'
+  }
+
+function checkConnection() {
+    let networkState = navigator.connection.type;
+
+    let states = {};
+
+    states[Connection.NONE] = 0;
+
+    if(states[networkState] == 0){
+      return false;
+    }else{
+      return true;
+    }
+}
+
+// checkConnection();
+
+function onConfirm(buttonIndex) {
+  if(buttonIndex == 1){
+    navigator.app.exitApp();
+  }else{
+    return false;
+  }
+}
+
 //ação de cadastrar uma pessoa e curso
 
-cadastrar.addEventListener("click", function(){
-  navigator.connection.type;
-   if(navigator.connection.type == Connection.NONE){
-     function onConfirm(buttonIndex){
-       function onConfirm(buttonIndex){
-         alert('You selected button '+ buttonIndex);
-       }
-     }
-     navigator.notification.confirm(
-       'voce está sem conexão,saia ou volte',
-       onConfirm,
-       'Sem Internet',
-       ['Tentar Novamente','Sair']
-     );
-   }else{
-    let formData = new FormData();
-    formData.append('nome', `${nome.value}`);
-    formData.append('curso', `${curso.value}`);
+cadastrar.addEventListener('click',function(){
+    if(checkConnection()){
+      let formdata = new FormData();
+      formdata.append('nome',`${nome.valueS}`);
+      formdata.append('curso',`${curso.value}`);
 
-    fetch("https://www.jussimarleal.com.br/exemplo_api/pessoa",
-        {
-            body: formData,
-            method: "post",
-            mode: 'cors',
-            cache: 'default'
-        }).then(alert("Cadastrado"));
-    }
-  });
-
-    //metodo que lista uma pessoa
- buscar.addEventListener("click", function(){
-   navigator.connection.type;
-   if(navigator.connection.type == Connection.NONE){
-     function onConfirm(buttonIndex){
-       function onConfirm(buttonIndex){
-         alert('You selected button '+ buttonIndex);
-       }
-     }
-     navigator.notification.confirm(
-       'voce está sem conexão,saia ou volte',
-       onConfirm,
-       'Sem Internet',
-       ['Tentar Novamente','Sair']
-     );
-   }else{
-
-    fetch(`https://www.jussimarleal.com.br/exemplo_api/pessoa/${id.value}`,
-    {
-      method:'GET',
-      mode: 'cors',
-      cache: 'default'
-    })
-      .then(response => {response.json()
-        .then(data => {
-          document.querySelector("#nome").value = data['nome'];
-          document.querySelector("#curso").value = data['curso'];
-        })
-      });
-     }
-  });
-
-    //metodo para deletar o registro 
-
-    deletar.addEventListener("click", function(){
-      navigator.connection.type;
-   if(navigator.connection.type == Connection.NONE){
-     function onConfirm(buttonIndex){
-       function onConfirm(buttonIndex){
-         alert('You selected button '+ buttonIndex);
-       }
-     }
-     navigator.notification.confirm(
-       'voce está sem conexão,saia ou volte',
-       onConfirm,
-       'Sem Internet',
-       ['Tentar Novamente','Sair']
-     );
-   }else{
-      fetch(`https://www.jussimarleal.com.br/exemplo_api/pessoa/${id.value}`,{
-        method:"GET",
+      fetch("https://www.jussimarleal.com.br/exemplo_api/pessoa",
+      {
+        body: formdata,
+        method:"POST",
         mode:'cors',
         cache:'default'
       }).then(()=>{
-      alert("Registro deletado")
-      limparCampos();
+        alert("Cadastro efetuado com sucesso!!");
+        limparCampo();
       });
-     }
-    })
+    }else{    
+        navigator.notification.confirm(
+          'Você está sem conexão! Deseja Sair?', // message
+          onConfirm,            // callback to invoke with index of button pressed
+          'Erro de conexão',           // title
+          ['SIM','NÃO']     // buttonLabels
+        );
+      } 
+  });
+
+
+
+    //metodo que lista uma pessoa
+      buscar.addEventListener('click',function(){
+          if(checkConnection()){
+            fetch(`https://www.jussimarleal.com.br/exemplo_api/pessoa/${ result.text }`,opcoes)
+              .then(response =>{response.json().then(data =>{
+                nome.value = data['nome'];
+                curso.value = data['curso'];
+              })
+            })
+          }else{    
+            navigator.notification.confirm(
+                'Você está sem conexão! Deseja Sair?', // message
+                onConfirm,            // callback to invoke with index of button pressed
+                'Erro de conexão',           // title
+                ['SIM','NÃO']     // buttonLabels
+            );
+          } 
+        });
+
+    //metodo para deletar o registro 
+
+    deletar.addEventListener('click',function(){
+    if(checkConnection()){
+      fetch(`https://www.jussimarleal.com.br/exemplo_api/pessoa/${id.value}`,{
+        method :"DELETE",
+        mode:'cors',
+        cache:'default'
+      }).then(()=>{
+        alert("Registro Alterado com Sucesso!");
+        limparCampo();
+      });
+    }else{    
+        navigator.notification.confirm(
+          'Você está sem conexão! Deseja Sair?', // message
+          onConfirm,            // callback to invoke with index of button pressed
+          'Erro de conexão',           // title
+          ['SIM','NÃO']     // buttonLabels
+        );
+      } 
+    
+  });
 
     //metodo para alterar os dados dos registros
-    alterar.addEventListener("click", function(){
-    navigator.connection.type;
-   if(navigator.connection.type == Connection.NONE){
-     function onConfirm(buttonIndex){
-       function onConfirm(buttonIndex){
-         alert('You selected button '+ buttonIndex);
-       }
-     }
-     navigator.notification.confirm(
-       'voce está sem conexão,saia ou volte',
-       onConfirm,
-       'Sem Internet',
-       ['Tentar Novamente','Sair']
-     );
-   }else{
-       fetch(`https://www.jussimarleal.com.br/exemplo_api/pessoa/${id.value}`,{
-        method:"PUT",
-        mode:'cors',
-        cache:'default',
-        headers:{
-          'Conntent-type': 'application/json; charset=UTF-8'
-        },
-        body:JSON.stringify({
-          'nome': `${nome.value}`,
-          'curso': `${curso.value}`
-        })
-      }).then(()=>{
-        alert("registro alterado com sucesso")
-        limparCampos();
-      });
+      alterar.addEventListener('click',function(){
+     if(checkConnection()){
+        fetch(`https://www.jussimarleal.com.br/exemplo_api/pessoa/${id.value}`,{
+          method :"PUT",
+          mode:'cors',
+          cache:'default',
+          headers:{
+            'Content-type':'application/json; charset= UTF-8'
+          },
+          body:JSON.stringify({
+            'nome':`${nome.value}`,
+            'curso' : `${curso.value}`
+          })
+        }).then(()=>{
+          alert("Registro Alterado com Sucesso");
+          limparCampo();
+        });
+    }else{    
+        navigator.notification.confirm(
+          'Você está sem conexão! Deseja Sair?', // message
+          onConfirm,            // callback to invoke with index of button pressed
+          'Erro de conexão',           // title
+          ['SIM','NÃO']     // buttonLabels
+        );
+      } 
+  });
+
+//METODO BUSCAR CM QRCODE
+
+ buscarQR.addEventListener('click',function(){
+    if(checkConnection()){
+      cordova.plugins.barcodeScanner.scan(
+        function (result) {
+          fetch(`https://www.jussimarleal.com.br/exemplo_api/pessoa/${ result.text }`,opcoes)
+          .then(response =>{response.json().then(data =>{
+              nome.value = data['nome'];
+              curso.value = data['curso'];
+            })
+          })
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+        preferFrontCamera : false, // iOS and Android
+        showFlipCameraButton : false, // iOS and Android
+        showTorchButton : true, // iOS and Android
+        torchOn: false, // Android, launch with the torch switched on (if available)
+        saveHistory: false, // Android, save scan history (default false)
+        prompt : "Area de Scan", // Android
+        resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+        formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+        orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+        disableAnimations : true, // iOS
+        disableSuccessBeep: true // iOS and Android
       }
-    })
+      );
+    }else{
+      navigator.notification.confirm(
+          'Você está sem conexão! Deseja Sair?', // message
+          onConfirm,            // callback to invoke with index of button pressed
+          'Erro de conexão',           // title
+          ['SIM','NÃO']     // buttonLabels
+      );
+    }
+  });
+
 
     //ação de limpar os campos
     function limparCampos (){
       nome.value = "";
       curso.value = "";
     }
-    buscarQR.addEventListener("click", function(){
-      cordova.plugins.barcodeScanner.scan(
-        function (result) {
-            id.value = result.text;
-        },
-        function (error) {
-            alert("Scanning failed: " + error);
-        },
-      {
-          preferFrontCamera : false, // iOS and Android
-          showFlipCameraButton : true, // iOS and Android
-          showTorchButton : true, // iOS and Android
-          torchOn: false, // Android, launch with the torch switched on (if available)
-          saveHistory: true, // Android, save scan history (default false)
-          prompt : "Place a barcode inside the scan area", // Android
-          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-          formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-          orientation : "portait", // Android only (portrait|landscape), default unset so it rotates with the device
-          disableAnimations : true, // iOS
-          disableSuccessBeep: false // iOS and Android
-        }
-      );
-    });
   }
